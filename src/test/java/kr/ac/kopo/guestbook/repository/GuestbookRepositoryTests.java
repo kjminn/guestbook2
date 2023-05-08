e59140335129eaf76b315296443c01c75874b879
kjminn <kjminn2001@gmail.com>
2023-05-09 00:18:13 +0900

    querydsl관련 build.gradle 파일 수정

--- src/test/java/kr/ac/kopo/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/kr/ac/kopo/guestbook/repository/GuestbookRepositoryTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class GuestbookRepositoryTests {
     @Autowired
     private GuestbookRepository guestbookRepository;
+
 
     @Test
     public void insertDummies(){
@@ -22,5 +24,18 @@ public class GuestbookRepositoryTests {
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
         });
+    }
+
+    @Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed Title....");
+            guestbook.changeContent("Changed Content...");
+
+            guestbookRepository.save(guestbook);
+        }
     }
 }
